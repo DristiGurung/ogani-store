4fb5a6e13270b5b91cc7323ea51c09f83754df6e
DristiGurung <dristigurung1@gmail.com>
2023-06-04 13:49:14 +0545

    first init

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -17,6 +17,7 @@ class ProductController extends Controller
             'products' => $products
         ]);
     }
+    //init
     public function show($slug)
     {
         $product = Product::where('slug', $slug)->first();
